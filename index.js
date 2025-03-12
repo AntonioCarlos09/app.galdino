4da999f9debf7f4b555b599ee021ea53bc745037
Antonio Carlos <antoniocarlos07aaa@gmail.com>
2025-03-12 16:07:36 -0300

    essa está pronta

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function calcular() {
     const valor = parseFloat(document.getElementById('valor').value);
+    const porcentagem = parseFloat(document.getElementById('porcentagem').value);
 
     if (!isNaN(valor)) {
-        const resultado = valor + (valor * 0.50); // Soma 50% do valor
+        const resultado = valor + (valor * porcentagem); // Soma a porcentagem selecionada
         document.getElementById('resultado').textContent = `Resultado: R$ ${resultado.toFixed(2)}`;
     } else {
         document.getElementById('resultado').textContent = 'Por favor, insira um valor válido!';
